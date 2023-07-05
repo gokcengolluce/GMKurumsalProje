57ca739260204a909b5ddce979aeece06ec7562a
Gökçen Göllüce <gollucegokcen@gmail.com>
2023-07-05 17:39:29 +0300

    GM Kurumsal Web Sitesi
    kurumsal web sitesi

---
+++ GMKurumsalProje/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GMKurumsalProje.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public  ActionResult Aboutus()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Products()
+        {
+            return View();
+        }
+        public ActionResult Blog()
+        {
+            return View();
+        }
+        public ActionResult blogPost1()
+        {
+            return View();
+        }
+        public ActionResult blogPost2()
+        {
+            return View();
+        }
+        public ActionResult blogPost3()
+        {
+            return View();
+        }
+        public ActionResult blogPost4()
+        {
+            return View();
+        }
+        public ActionResult blogPost5()
+        {
+            return View();
+        }
+        public ActionResult blogPost6()
+        {
+            return View();
+        }
+
+
+    }
+}
